f6e3f7e5ae68fd12ee9f96b1567e8165de182a6c
abumalick <abdullah.hilson@gmail.com>
2026-01-31 16:31:26 +0300

    release v1.2.0: gallery QR scanning

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "net.hilson.qrieux"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
     }
 
     signingConfigs {
