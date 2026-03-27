e4cec1aafeeae8adf73314bb7982f8a16fdafb5e
abumalick <abdullah.hilson@gmail.com>
2026-03-27 11:06:37 +0300

    bump v1.7.1: fix share extension open app + vCard priority

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,8 +72,8 @@ android {
         applicationId = "net.hilson.qrieux"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "1.7.0"
+        versionCode = 14
+        versionName = "1.7.1"
     }
 
     signingConfigs {
