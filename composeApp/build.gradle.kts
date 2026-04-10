7ccf2d77450652bf92935d3eaf0f74c380b14831
abumalick <abdullah.hilson@gmail.com>
2026-04-10 18:13:01 +0300

    bump version to 1.8.2 + fix macOS Tahoe fastlane PTY issue

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,8 +72,8 @@ android {
         applicationId = "net.hilson.qrieux"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 15
-        versionName = "1.8.0"
+        versionCode = 22
+        versionName = "1.8.2"
     }
 
     signingConfigs {
